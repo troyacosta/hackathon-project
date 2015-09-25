10fbd9319c7eec9d235f5d2d0881f98e4bc0a3b6
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-25 13:43:25 -0500

    push js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,59 @@
 'use strict';
 var $ = require('jquery');
+var Backbone = require('backbone');
+
+$(document).ready(function() {
+	
+    var $logIn = $('#logIn');
+    var $loginForm = $('.loginForm');
+    var $home = $('#home');
+    var $employees = $('#employees');
+    var $singleEmployee = $('#singleEmployee');
+    var $editEmployeeInfo = $('#editEmployeeInfo');
+    var $addNewEmployee = $('#addNewEmployee');
+    var $all = $('.all');
+
+   // $all.hide();
+
+    $loginForm.on('submit', function(event) {
+		event.preventDefault();
+	});
+
+    var Router = Backbone.Router.extend({
+        routes: {
+            'logIn': 'logIn',
+            'home': 'home',
+            'employees': 'employees',
+            'singleEmployee': 'singleEmployee',
+            'editEmployeeInfo': 'editEmployeeInfo',
+            'addNewEmployee': 'addNewEmployee'
+        },
+        logIn: function() {
+        	$all.hide;
+        	$loginForm.show();
+        },
+        home: function() {
+        	$all.hide();
+            $home.show();
+        },
+        employees: function() {
+        	$all.hide();
+        	$employees.show();
+        }
+        singleEmployee: function() {
+        	$all.hide();
+        	$singleEmployee.show();
+        },
+        editEmployeeInfo: function() {
+        	$all.hide();
+        	$editEmployeeInfo.show();
+        },
+        addNewEmployee: function() {
+        	$all.hide();
+        	$editEmployeeInfo.show();
+        }
+    });
+    var router = new Router;
+
+    Backbone.history.start();
+});
